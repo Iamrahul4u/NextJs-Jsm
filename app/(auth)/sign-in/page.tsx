40abba3b370f3071c66d1c9638c72b43d5ee04fd
Rahul Gupta <iamrahulgupta4u@gmail.com>
2023-10-28 22:13:37 +0530

    initial routing

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,3 @@
+export default function page() {
+  return <div className="h3-bold  flex flex-1 ">sign in</div>;
+}
--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <div className="flex  flex-1 bg-black">Hello world</div>;
-}
