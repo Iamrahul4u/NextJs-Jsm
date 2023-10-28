17a99ef59d41b3bdae78c317a7c9ee893e843fe6
Rahul Gupta <iamrahulgupta4u@gmail.com>
2023-10-28 23:27:18 +0530

    layouts and assets included

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,21 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
 import "../globals.css";
+import { Inter, Space_Grotesk } from "next/font/google";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description: "A community-driven Platform for asking and answering questions",
+};
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
+const grotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 export default function RootLayout({
   children,
@@ -10,6 +25,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <h1>Home</h1>
         <body>{children}</body>
       </html>
     </ClerkProvider>
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,45 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
 import "../globals.css";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description: "A community-driven Platform for asking and answering questions",
+};
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-inter",
+});
+const grotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${grotesk.variable}`}>
+          <h1 className="h1-bold">HomeNav</h1>
+
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
