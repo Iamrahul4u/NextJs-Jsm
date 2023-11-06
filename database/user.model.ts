2fcd41266fa1afd1350f76724963cff410666591
Rahul Gupta <iamrahulgupta4u@gmail.com>
2023-11-06 18:54:52 +0530

    user schema fixed

--- database/user.model.ts
+++ database/user.model.ts
@@ -1,72 +1,71 @@
-import  {model,models, Schema , Document } from "mongoose";
+import { model, models, Schema, Document } from "mongoose";
 
-
-export interface IUser extends Document{
-   clerkid:string;
-   name:string;
-   username:string;
-   email:string;
-   bio?:string;
-    password?:string;
-    reputation?:number;
-    picture?:string;
-    location?:string;
-    saved:Schema.Types.ObjectId[];
-    joinedAt:Date;
-    portfolioWebsite:string;
+export interface IUser extends Document {
+  clerkid: string;
+  name: string;
+  username: string;
+  email: string;
+  bio?: string;
+  password?: string;
+  reputation?: number;
+  picture?: string;
+  location?: string;
+  saved?: Schema.Types.ObjectId[];
+  joinedAt?: Date;
+  portfolioWebsite?: string;
 }
 
-
-
 const userSchema = new Schema<IUser>({
-    clerkid: {
-        type: String,
-        required: true,
-        unique: true,
+  clerkid: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  bio: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  reputation: {
+    type: Number,
+    default: 0,
+  },
+  picture: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  saved: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Question", // Reference to the Question model or another applicable model
     },
-    name: {
-        type: String,
-        required: true,
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    bio: {
-        type: String,
-    },
-    password: {
-        type: String,
-    },
-    reputation: {
-        type: Number,
-        default:0
-    },
-    picture: {
-        type: String,
-    },
-    location: {
-        type: String,
-    },
-    saved: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Question', // Reference to the Question model or another applicable model
-    }],
-    joinedAt: {
-        type: Date,
-        required: true,
-    },
-    portfolioWebsite: {
-        type: String,
-    },
+  ],
+  joinedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  portfolioWebsite: {
+    type: String,
+  },
 });
 
-const User = models.User||model<IUser>('User', userSchema);
+const User = models.User || model<IUser>("User", userSchema);
 
-export {User};
+export { User };
